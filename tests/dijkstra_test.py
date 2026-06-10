5b1e68317bb4a0ff9c67597684147c107bbcd779
Kacper Łucki <klucki2003@gmail.com>
2026-06-10 21:09:26 +0200

    Fix dijkstra_test.py path issue

--- tests/dijkstra_test.py
+++ tests/dijkstra_test.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+import sys
 import unittest
 from math import inf
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(PROJECT_ROOT))
 
 from dijkstra import Graph, dijkstra, total_cost
 

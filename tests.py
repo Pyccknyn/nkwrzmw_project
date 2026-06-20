02b2e77994c95060d4dc3f37885455d2a387fe9e
ER562 <xeryk96@gmail.com>
2026-06-20 15:38:27 +0200

    coś

--- tests.py
+++ tests.py
@@ -1,18 +1,25 @@
 import dijkstra as dj
+import re
 
-g = dj.Graph_lite(4)
-g.add_edge(1, 2, 1.0)
-g.add_edge(2, 3, 2.0)
-g.add_edge(1, 3, 5.0)  # Powinna być NIEUŻYWANA
-g.add_edge(3, 4, 1.0)
-g.add_edge(1, 4, 7.0)  # Powinna być NIEUŻYWANA
+g1 = dj.Graph_lite(20)  # Zmieniłem na 7, aby pomieścić wierzchołki od 1 do 6
+g2 = dj.Graph(20)
+
+with open("dane.txt", "r", encoding="utf-8") as file:
+    pattern = r"(\d+),(\d+),(\d+)"
+    for line in file:
+        match = re.search(pattern, line)
+        if match:
+            source, target, weight = match.groups()
+            g1.add_edge(int(source), int(target), int(weight))  # <- TUTAJ: Dodatkowe wcięcie!
+            g2.add_edge(int(source), int(target), float(weight))
+
 
 print("--- ORYGINALNY GRAF ---")
-g.print_edges()
+g1.print_edges()
 
-distances = dj.dijkstra_lite(g, src=1)
+distances = dj.dijkstra_lite(g1, src=1)
 
-nieuzywane_listy = dj.unused_connections(g, distances)
+nieuzywane_listy = dj.unused_connections(g1, distances)
 
 print("\n--- POZOSTAŁE (NIEUŻYWANE) KRAWĘDZIE ---")
 for i in range(1, len(nieuzywane_listy)):
